04c1f424c2ee466ee329dc822ba5cd5ded31caa3
Qidi He <hejacky28@gmail.com>
2013-04-04 17:39:34 -0400

    Add User Model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module App1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # Required for heroku devise
+    config.assets.initialize_on_precompile = false
   end
 end
