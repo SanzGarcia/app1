96ecd2bace4f126c4fdc181e366181b6ac8f5839
Qidi He <hejacky28@gmail.com>
2013-04-05 14:22:33 -0400

    Add Pins Scaffold

---
+++ app/models/pin.rb
@@ -0,0 +1,5 @@
+class Pin < ActiveRecord::Base
+  attr_accessible :description #, :string
+
+  validates :description, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 App1::Application.routes.draw do
+  resources :pins
+
+
   devise_for :users
 
   get 'about' => 'pages#about'
